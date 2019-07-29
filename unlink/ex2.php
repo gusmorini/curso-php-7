87f85b8132e65a60cd4412fb3a8dae0c62eb61f9
Gustavo Morini <gustavommh@gmail.com>
2019-07-29 16:30:58 -0300

    unlink

--- unlink/ex2.php
+++ unlink/ex2.php
@@ -1,3 +1,17 @@
 <?php
 
-mkdir("images");
+echo "<h1>Deleta arquivos</h1>";
+
+if (!is_dir("images")) mkdir("images");
+
+foreach (scandir("images") as $item) {
+    if (!in_array($item, array(".",".."))) {
+
+        unlink ("images/" . $item );
+        
+        echo "$item removido <br>";
+
+    }
+}
+
+//apaga todos os arquivos da pasta images
