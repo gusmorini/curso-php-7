ce530961e929f4c36f40d978323f67c563460cde
Gustavo Morini <gustavommh@gmail.com>
2019-08-08 18:26:02 -0300

    gd

---
+++ gd/ex4.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Content-Type: image/jpg");
+
+$file = "wallpaper.jpg";
+
+$new_width = 256;
+$new_height = 256;
+
+list($old_width, $old_height) = getimagesize($file);
+
+$new_image = imagecreatetruecolor($new_width, $new_height);
+$old_image = imagecreatefromjpeg($file);
